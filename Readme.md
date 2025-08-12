b3d9981b393c8dc59fbf5c8f01a1c332fcd0c1c7
dmoprates <prates.diego@gmail.com>
2025-08-12 13:54:41 -0300

    chore: Início do projeto de um CMS Avançado - Aula do curso Desenvolvimento Web Completo - Danki Code

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# CMS Avançado
+
+- Sistema criado no Curso "Desenvolvimento Web Completo - Danki Code"
+- Linguagens de programação estudadas:
+    - PHP;
+    - HTML;
+    - CSS.
+
+Créditos para os templates à Danki Code
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Painel CMS</title>
+</head>
+
+<body>
+    <header>
+        <div class="center">
+            <div class="logo"><a href="">Painel CMS</a></div>
+            <div class="menu">
+                <a href="">Cadastrar página</a>
+                <a href="">Listar páginas</a>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </header>
+</body>
+
+</html>
